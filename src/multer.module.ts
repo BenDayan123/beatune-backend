9e47f57963f8acdc679b07da2b1231e4a70e14be
Ben Dayan <ben.dayan123@gmail.com>
2024-01-13 15:54:49 +0200

    deploy to render

--- src/multer.module.ts
+++ src/multer.module.ts
@@ -6,7 +6,7 @@ import { extname } from 'path';
 export const MulterModuleConfig = () => {
   return MulterModule.register({
     storage: diskStorage({
-      destination: './src/media',
+      destination: '/src/media',
       filename: (_, file, callback) => {
         const filetype = extname(file.originalname);
         callback(null, `${randomUUID()}${filetype}`);
